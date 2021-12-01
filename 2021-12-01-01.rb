4e3fe9fabeaf82c87c8f0bac3bde420946d3f749
Prime <isabel@thesandins.net>
2021-12-01 12:39:53 -0800

    That's good enough for 2021-12-01-01

--- 2021-12-01-01.rb
+++ 2021-12-01-01.rb
@@ -9,8 +9,18 @@ input_fh = open ARGV[0]
 input_str = input_fh.read
 input_fh.close
 
-# pp input_str
-
 inputs = input_str.split(/\n/).map{|i| i.to_i}
 
-pp inputs
+# pp inputs
+
+increases = 0
+last_number_seen = (2 << 100) # I hope that's practically close to infinite
+inputs.each { |i|
+  if i > last_number_seen
+    increases += 1
+  end
+  # printf "i: %d last_number_seen: %d\n", i, last_number_seen
+  last_number_seen = i
+}
+
+printf "Found %d increases in %s\n", increases, ARGV[0]
