2b2d09cb6937123f45889080addf04629943992f
Prime <isabel@thesandins.net>
2021-12-01 15:18:32 -0800

    Break 2021-12-01-02 up into methods

--- 2021-12-01-02.rb
+++ 2021-12-01-02.rb
@@ -5,31 +5,42 @@ if ARGV.length == 0
   exit
 end
 
-input_fh = open ARGV[0]
-input_str = input_fh.read
-input_fh.close
+def slurp_array (path)
+  input_fh = open path
+  input_str = input_fh.read
+  input_fh.close
 
-inputs = input_str.split(/\n/).map{|i| i.to_i}
-
-slice_size = 3
-
-if slice_size > inputs.length
-  puts 'Well this is bad, gtfo'
-  exit
+  return input_str.split(/\n/).map{|i| i.to_i}
 end
 
-slice_count = inputs.length - slice_size
-blocks = (0..slice_count).map { |i|
-  inputs.slice(i, slice_size).sum
-}
-
-increases = 0
-last_number_seen = (2 << 100) # I hope that's practically close to infinite
-blocks.each { |i|
-  if i > last_number_seen
-    increases += 1
+def bucket_inputs (data, slice_size)
+  if slice_size > data.length
+    puts 'Well this is bad, gtfo'
+    exit
   end
-  last_number_seen = i
-}
 
-printf "Found %d increases in %s with window size %d\n", increases, ARGV[0], slice_size
+  slice_count = data.length - slice_size
+  blocks = (0..slice_count).map { |i|
+    data.slice(i, slice_size).sum
+  }
+end
+
+def count_increases (data)
+  increases = 0
+  last_number_seen = (2 << 100) # I hope that's practically close to infinite
+  data.each { |i|
+    if i > last_number_seen
+      increases += 1
+    end
+    last_number_seen = i
+  }
+  return increases
+end
+
+ss = 3
+input_path = ARGV[0]
+inputs = slurp_array(input_path)
+blocks = bucket_inputs(inputs, ss)
+result = count_increases(blocks)
+
+printf "Found %d increases in %s with window size %d\n", result, input_path, ss
