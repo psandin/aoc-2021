b3d2501b7cc26027fc9e281ddd3285779d7b4a12
Prime <isabel@thesandins.net>
2021-12-05 23:07:33 -0800

    2021-12-06 part 2, aka don't simulate individual fish

--- 2021-12-06-02.rb
+++ 2021-12-06-02.rb
@@ -4,6 +4,9 @@ require 'optparse'
 $args = {}
 OptionParser.new do |opts|
   opts.on('-f PATH', '--file PATH', String, :REQUIRED)
+  opts.on('-t TICKS', '--ticks TICKS', Integer)
+  opts.on('-b BA', '--birthage BA', Integer)
+  opts.on('-d BD', '--delay TICKS', Integer)
   opts.on('-v', '--verbose')
 end.parse!(into: $args)
 raise OptionParser::MissingArgument, "--file" if $args[:file].nil?
@@ -15,3 +18,48 @@ def slurp (path)
 
   return input_str.split(/,/)
 end
+
+$fish_counter = 0
+$days_between_births = $args.key?(:birthage) ? $args[:birthage].to_i : 6
+$zero_cycle_delay = $args.key?(:delay) ? $args[:delay].to_i : 2
+$tick_count = $args.key?(:ticks)  ? $args[:ticks].to_i : 80
+
+puts "Days between births set to #{$days_between_births}" if $args[:verbose]
+puts "Additional cycle zero delay set to #{$zero_cycle_delay}" if $args[:verbose]
+puts "Tick count set to #{$tick_count}" if $args[:verbose]
+
+$feeeesh_school = {}
+
+def load_feeeesh
+  0.upto($zero_cycle_delay + $days_between_births).each { |i|
+    $feeeesh_school[i] = 0
+  }
+  base_pop = slurp($args[:file])
+  base_pop.each { |f|
+   $feeeesh_school[f.to_i] += 1
+  }
+end
+
+def dump_school
+  puts $feeeesh_school if $args[:verbose]
+end
+
+def global_tick
+  birthing = $feeeesh_school[0]
+  1.upto($zero_cycle_delay + $days_between_births) { |i|
+    $feeeesh_school[i-1] = $feeeesh_school[i]
+  }
+  $feeeesh_school[$days_between_births] += birthing
+  $feeeesh_school[$days_between_births + $zero_cycle_delay] = birthing
+end
+
+load_feeeesh
+dump_school
+
+1.upto($tick_count).each {
+  global_tick
+  dump_school
+}
+
+total_feeesh = $feeeesh_school.each_value.sum
+puts "#{total_feeesh} total feeeesh"
