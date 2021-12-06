50dfcdc89ef68854b877db7903a6b5b804d847f5
Prime <isabel@thesandins.net>
2021-12-06 00:19:57 -0800

    Day 5 prep

---
+++ 2021-12-05-02.rb
@@ -0,0 +1,17 @@
+require 'pp'
+require 'optparse'
+
+$args = {}
+OptionParser.new do |opts|
+  opts.on('-f PATH', '--file PATH', String, :REQUIRED)
+  opts.on('-v', '--verbose')
+end.parse!(into: $args)
+raise OptionParser::MissingArgument, "--file" if $args[:file].nil?
+
+def slurp (path)
+  input_fh = open path
+  input_str = input_fh.read
+  input_fh.close
+
+  return input_str.split(/\n/)
+end
