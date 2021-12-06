d4431a343b4ba608dd19457888f210dc7e7a6188
Prime <isabel@thesandins.net>
2021-12-06 01:17:58 -0800

    2021-12-06 part 1, basic lines

--- 2021-12-05-01.rb
+++ 2021-12-05-01.rb
@@ -15,3 +15,68 @@ def slurp (path)
 
   return input_str.split(/\n/)
 end
+
+def parse_line(line)
+  wat = line.split(/ -> /).map do |l|
+    pair = l.split(/,/)
+    {x: pair[0].to_i, y:pair[1].to_i}
+  end
+end
+
+$vents = {}
+def mark_line(line)
+  if line[0][:x] == line[1][:x]
+    smaller, larger = [line[0][:y], line[1][:y]].minmax
+    puts "found verticle line #{line}" if $args[:verbose]
+    (smaller..larger).each do |e|
+      point = [line[0][:x],e]
+      puts "Marking point #{point}" if $args[:verbose]
+      $vents[point] = 0 if $vents[point].nil?
+      $vents[point] += 1
+    end
+    return true
+  end
+  if line[0][:y] == line[1][:y]
+    smaller, larger = [line[0][:x], line[1][:x]].minmax
+    puts "found horizontal line #{line}" if $args[:verbose]
+    (smaller..larger).each do |e|
+      point = [e,line[0][:y]]
+      puts "Marking point #{point}" if $args[:verbose]
+      $vents[point] = 0 if $vents[point].nil?
+      $vents[point] += 1
+    end
+    return true
+  end
+  puts "Discarding line #{line}" if $args[:verbose]
+  return false
+end
+
+def render_vents
+  puts
+  (0..9).each do |y|
+    (0..9).each do |x|
+      print $vents.key?([x,y]) ? $vents[[x,y]] : '.'
+    end
+    puts
+  end
+  puts
+end
+
+def count_vents
+  count = 0
+  $vents.each_value do |v|
+    count += 1 if v > 1
+  end
+  return count
+end
+
+lines = slurp($args[:file]).map {|l| parse_line(l)}
+puts "#{lines}" if $args[:verbose]
+puts if $args[:verbose]
+lines.each do |l|
+  if mark_line(l)
+     render_vents if $args[:verbose]
+  end
+end
+
+puts "Double vents: #{count_vents}"
