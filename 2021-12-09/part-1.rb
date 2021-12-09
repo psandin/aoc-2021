846d1acb0ebc8949203090125dfc95a998e290c1
Prime <isabel@thesandins.net>
2021-12-08 21:24:32 -0800

    Day 9 part 1

--- 2021-12-09/part-1.rb
+++ 2021-12-09/part-1.rb
@@ -24,4 +24,50 @@ def slurp (path)
   return input_str.split(/\n/)
 end
 
-raw_lines = slurp($args[:file])
+def find_low_points (height_map)
+  max_y = height_map.length - 1
+  max_x = height_map[0].length - 1
+  puts "max y #{max_y}"
+  puts "max x #{max_x}"
+  mins = []
+  height_map.each_with_index do |r,i|
+    r.each_with_index do |e, j|
+      print "judging #{e} @ (#{j}, #{i}) "
+      taller = 0
+      if j == 0
+        taller += 1
+      elsif height_map[i][j-1] > e
+        taller += 1
+      end
+
+      if i == 0
+        taller += 1
+      elsif height_map[i-1][j] > e
+        taller += 1
+      end
+
+      if j == max_x
+        taller += 1
+      elsif height_map[i][j+1] > e
+        taller += 1
+      end
+
+      if i == max_y
+        taller += 1
+      elsif height_map[i+1][j] > e
+        taller += 1
+      end
+
+      puts "score: #{taller}"
+      mins.push(e.to_i) if taller == 4
+    end
+  end
+  return mins
+end
+
+height_map = slurp($args[:file]).map {|l| l.split(//)}
+mins = find_low_points(height_map)
+puts "#{mins}"
+solution = mins.sum + mins.length
+puts "solution #{solution}"
+
