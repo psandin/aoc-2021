217058d7581190ce8ff515223cac7ff19310ea10
Prime <isabel@thesandins.net>
2021-12-09 21:59:56 -0800

    Day 9 part two

--- 2021-12-10/part-2.rb
+++ 2021-12-10/part-2.rb
@@ -24,4 +24,58 @@ def slurp (path)
   return input_str.split(/\n/)
 end
 
+@expected = {}
+@expected['('] = ')'
+@expected['{'] = '}'
+@expected['['] = ']'
+@expected['<'] = '>'
+
+# puts "#{@expected}"
+
+def validate(line)
+  expected_closers = []
+  bad_char = nil
+  line.chars.each do |c|
+    # puts "Pondering #{c}"
+    if @expected.has_key?(c)
+      expected_closers.push(@expected[c])
+      # puts "Pushed: #{expected_closers}"
+    elsif expected_closers.last == c
+      expected_closers.pop
+      # puts "Poped: #{expected_closers}"
+    else
+      # puts "Expected #{expected_closers.last} found #{c}"
+      bad_char = c
+      break
+    end
+  end
+  if bad_char.nil?
+    puts "#{score_complete(expected_closers)}"
+    score_complete(expected_closers)
+  else
+    0
+  end
+end
+
+def score_complete (charset)
+  mul = charset.length + 1
+  mul = 5
+  charset = charset.reverse
+
+  values = {}
+  values[')']=1
+  values[']']=2
+  values['}']=3
+  values['>']=4
+
+  charset.reduce do |o, i|
+    o = values[o] if o.is_a? String
+    (o * 5) + values[i]
+  end
+end
+
 raw_lines = slurp($args[:file])
+scores = raw_lines.map { |l| validate(l) }.select { |i| i!=0 }.sort
+puts "#{scores}"
+puts scores[scores.length / 2]
+
