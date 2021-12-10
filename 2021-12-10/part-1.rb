061e8f6e96dc88da1433e5883453c4257eade5d9
Prime <isabel@thesandins.net>
2021-12-09 21:59:46 -0800

    Day 9 part one

--- 2021-12-10/part-1.rb
+++ 2021-12-10/part-1.rb
@@ -24,4 +24,48 @@ def slurp (path)
   return input_str.split(/\n/)
 end
 
+@expected = {}
+@expected['('] = ')'
+@expected['{'] = '}'
+@expected['['] = ']'
+@expected['<'] = '>'
+
+puts "#{@expected}"
+
+def validate(line)
+  expected_closers = []
+  bad_char = nil
+  line.chars.each do |c|
+    # puts "Pondering #{c}"
+    if @expected.has_key?(c)
+      expected_closers.push(@expected[c])
+      # puts "Pushed: #{expected_closers}"
+    elsif expected_closers.last == c
+      expected_closers.pop
+      # puts "Poped: #{expected_closers}"
+    else
+      puts "Expected #{expected_closers.last} found #{c}"
+      bad_char = c
+      break
+    end
+  end
+  return bad_char
+end
+
+def score_errors(char)
+  case char
+  when ')'
+    3
+  when ']'
+    57
+  when '}'
+    1197
+  when '>'
+    25137
+  else
+    0
+  end
+end
 raw_lines = slurp($args[:file])
+score = raw_lines.map { |l| validate(l) }.select { |i| not i.nil? }.map{ |c| score_errors(c)}.sum
+puts "#{score}"
