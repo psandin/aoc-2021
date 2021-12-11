23d32d4d912fa6db3e22314fbfc0194b63e891d4
Prime <isabel@thesandins.net>
2021-12-10 22:34:05 -0800

    Day 11 part two

--- 2021-12-11/part-2.rb
+++ 2021-12-11/part-2.rb
@@ -1,5 +1,7 @@
 require 'pp'
 require 'optparse'
+require 'term/ansicolor'
+include Term::ANSIColor
 
 $args = {
   file: File.dirname(__FILE__) + "/input"
@@ -24,4 +26,56 @@ def slurp (path)
   return input_str.split(/\n/)
 end
 
-raw_lines = slurp($args[:file])
+def draw_array(arr)
+  s = Math.sqrt(arr.length)
+  arr.each_with_index do |v, i|
+    if v == 0
+      print "0 ".bold
+    else
+      print "#{v} "
+    end
+    puts if i % s == s - 1
+  end
+end
+
+def cycle(arr)
+  flashed = (0..arr.length-1).map { false }
+  (0..arr.length-1).each do |i|
+    next if flashed[i]
+    arr[i] += 1
+    flash_cell(i, arr, flashed) if arr[i] == 10
+  end
+  flashed.map {|c| c ? 1 : 0}.sum
+end
+
+def flash_cell(i, arr, flashed)
+  return if flashed[i]
+  arr[i] = 0
+  flashed[i] = true
+  s = Math.sqrt(arr.length)
+
+  neighbors = [s, -s]
+  neighbors.push(1, s+1,-s+1) unless i % s == s -1
+  neighbors.push(-1,s-1,-s-1) unless i % s == 0
+
+  neighbors.each do |m|
+    if i+m >= 0 and i+m < arr.length
+      next if flashed[i+m]
+      arr[i+m] += 1
+      flash_cell(i+m, arr, flashed) if arr[i+m] == 10
+    end
+  end
+end
+
+linear_array = slurp($args[:file]).map { |l| l.chars.map { |c| c.to_i } }.reduce(:+)
+
+draw_array(linear_array) if $args[:verbose]
+
+cycle = 0
+flashed = 0
+while flashed != 100
+  flashed = cycle(linear_array)
+  draw_array(linear_array) if $args[:verbose]
+  cycle += 1
+  puts "#{cycle}"
+end
