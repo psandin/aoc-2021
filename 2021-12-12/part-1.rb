27d7b554f451bca82a849267ab40cd0c3db7fcc1
Prime <isabel@thesandins.net>
2021-12-12 01:49:21 -0800

    12p1

--- 2021-12-12/part-1.rb
+++ 2021-12-12/part-1.rb
@@ -1,5 +1,6 @@
 require 'pp'
 require 'optparse'
+require 'set'
 
 $args = {
   file: File.dirname(__FILE__) + "/input"
@@ -24,4 +25,47 @@ def slurp (path)
   return input_str.split(/\n/)
 end
 
-raw_lines = slurp($args[:file])
+def build_map(connections)
+  network = {}
+  connections.each do |e|
+    e.each do |s|
+      if network[s].nil?
+        network[s] = {
+          big: (s.match /[[:upper:]]/) ? true : false,
+          neighbors: []
+        }
+      end
+    end
+    network[e[0]][:neighbors].push(e[1])
+    network[e[1]][:neighbors].push(e[0])
+  end
+  return network
+end
+
+$frame_id = 0
+$gpaths = []
+
+def walk_paths(network, node: 'start', visited: {}, path: [])
+  $frame_id += 1
+  local_frame = $frame_id
+  path.push(node)
+  if node == 'end'
+    $gpaths.push(path)
+    return
+  end
+  local_node = network[node]
+  visited[node] = true unless local_node[:big]
+
+  local_node[:neighbors].each do |n|
+    next if visited[n]
+    walk_paths(network, node: n, visited: visited.clone, path: path.clone)
+  end
+end
+
+raw_lines = slurp($args[:file]).map { |l| l.split('-')}
+network = build_map(raw_lines)
+puts network
+walk_paths(network)
+puts
+puts "#{$gpaths}"
+puts "#{$gpaths.length}"
