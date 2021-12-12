d44f3459614d18acd59a8159a7a26875b9c2e140
Prime <isabel@thesandins.net>
2021-12-12 01:49:28 -0800

    12p2

--- 2021-12-12/part-2.rb
+++ 2021-12-12/part-2.rb
@@ -1,5 +1,7 @@
 require 'pp'
 require 'optparse'
+require 'term/ansicolor'
+include Term::ANSIColor
 
 $args = {
   file: File.dirname(__FILE__) + "/input"
@@ -24,4 +26,60 @@ def slurp (path)
   return input_str.split(/\n/)
 end
 
-raw_lines = slurp($args[:file])
+def build_map(connections)
+  network = {}
+  connections.each do |e|
+    e.each do |s|
+      if network[s].nil?
+        network[s] = {
+          big: (s.match /[[:upper:]]/) ? true : false,
+          neighbors: []
+        }
+      end
+    end
+    network[e[0]][:neighbors].push(e[1])
+    network[e[1]][:neighbors].push(e[0])
+  end
+  return network
+end
+
+$gpaths = []
+
+def walk_paths(network, node: 'start', visited: {}, path: [], two_burned: '')
+  path.push(node)
+  if node == 'end'
+    $gpaths.push(path)
+    return
+  end
+  local_node = network[node]
+  prefix = path.join(',')
+  unless local_node[:big]
+    visited[node] = 0 if visited[node].nil?
+    visited[node] += 1
+    if visited[node] > 1 and two_burned == ''
+      two_burned = prefix.clone
+    end
+    visited[node] += 1 if node == "start"
+  end
+
+  local_node[:neighbors].each do |n|
+    if not visited[n].nil? and ((visited[n] > 1 and two_burned == '') or (visited[n] > 0 and two_burned != ''))
+      next
+    end
+    walk_paths(network, node: n, visited: visited.clone, path: path.clone, two_burned: two_burned.clone)
+  end
+end
+
+raw_lines = slurp($args[:file]).map { |l| l.split('-')}
+network = build_map(raw_lines)
+puts network
+walk_paths(network)
+$gpaths.each do |p|
+  puts  p.join(',')
+end
+puts "#{$gpaths.length}"
+
+
+
+
+
