5b710d55a8122e102510a1fd1ba27fd224072ae1
Prime <isabel@thesandins.net>
2021-12-16 18:19:08 -0800

    Fast enough, the priority queue bit is no joke, don't try to calculate priority on the fly

--- 2021-12-15/part_2.rb
+++ 2021-12-15/part_2.rb
@@ -36,6 +36,28 @@ def slurp_and_parse(path)
   [linear, size]
 end
 
+# They weren't bluffing about using a priority queue, so we shall
+class PQueue
+  def initialize
+    @storage = Hash.new { |h, k| h[k] = [] }
+  end
+
+  def push (item, priority)
+    @storage[priority].push(item)
+  end
+
+  def next
+    priority, bucket = @storage.min_by {|k,_| k }
+    return nil if bucket.nil?
+    @storage.delete(priority) if bucket.length == 1
+    bucket.shift
+  end
+
+  def has_items?
+    !@storage.first.nil?
+  end
+end
+
 def scale_up(graph, size, factor)
   staging = {}
   graph.each_with_index do |e, i|
@@ -61,96 +83,61 @@ def neighbors(node, size)
 end
 
 def manhattan (start, goal)
-  # return 0
-  # puts "==> manhattan(#{start}, #{goal})"
   s = Math.sqrt(goal + 1)
   x = (start / s).to_i
   y = (start % s).to_i
-  # puts "s => #{s}"
-  # puts "x => #{x}"
-  # puts "y => #{y}"
   Math.sqrt((((s - y)**2 + (s - x)**2))).round * 2
 end
 
-def render_search_area(searched, graph)
-  # puts searched.to_s
-  figs = 5
-  s = Math.sqrt(graph.length)
-  (0..(s-1)).each do |y|
-    (0..(s-1)).each do |x|
-      i = ((s * y) + x).to_i
-      # puts "pulling data about #{i}"
-      node = searched[i]
-      # puts "Found #{node}"
-      if node.nil?
-        print " ".rjust(figs+1,'-')
-      else
-        cost = node[:cost] + manhattan(i, graph.length - 1) 
-        v = "#{cost.to_s.rjust(figs, '0')} "
-        v = v.bold if node[:in_spt]
-        print v
-      end
-    end
-    puts
-  end
-  puts
+def calc_priority(new_cost, index, size)
+  goal = (size**2)-1
+  m = manhattan(index, goal)
+  new_cost + m
 end
 
-
-def dijkstra_spt(graph, size)
-  distances = { 0 => { cost: 0, path: [], in_spt: false } }
-  nodes_touched = 0
-  until graph.length == distances.select { |_, v| v[:in_spt] }.length
-    found = distances.select { |_, v| v[:in_spt] }.length
-    to_find = distances.reject { |_, v| v[:in_spt] }
-    best_candidates = to_find.sort_by {|k, v| manhattan(k, graph.length - 1) }[0, (graph.length * 1).to_i]
-    position, node = best_candidates.min_by { |k, v| v[:cost]}
-    if (found % 100).zero?
-      print "#{found}/#{graph.length} => #{((found / graph.length.to_f) * 100).round(3)}% "
-      m = manhattan(position, graph.length - 1)
-      mm = manhattan(0, graph.length - 1)
-      mp = (((mm - m.to_f)/mm)*100).round(3)
-      puts  "m: #{m} mp: #{mp}%"
-    end
-    node[:in_spt] = true
-    neighbors(position, size).each do |i|
-      neighbor_node = distances[i]
-      next if !neighbor_node.nil? && neighbor_node[:in_spt]
-
-      new_cost = node[:cost] + graph[i]
-      new_path = node[:path].clone.push(i)
-      if neighbor_node.nil? || neighbor_node[:cost] > new_cost
-        distances[i] = { cost: new_cost, path: new_path, in_spt: false }
-      end
-    end
-    nodes_touched += 1
-    # render_search_area(distances, graph)
-    break if position == graph.length - 1
+def a_star(graph, size)
+  goal = (size**2) - 1
+  candidate_queue = PQueue.new()
+  candidate_queue.push(0, 0)
+  path_data = Hash.new do |h, k|
+    h[k] = Hash.new()
+    h[k] = { cost:2**32, path: [], finalized: false }
   end
-  puts "Touched #{nodes_touched} of #{graph.length} nodes (#{((nodes_touched.to_f/graph.length)*100).round(3)}%)"
-  distances[(size * size) - 1][:path]
+  path_data[0] = { cost: 0, path: [], finalized: false }
+
+  iterations = 0
+  while candidate_queue.has_items?
+    current_index = candidate_queue.next
+    current_data = path_data[current_index]
+    next if current_data[:finalized]
+    current_data[:finalized] = true
+
+    neighbors(current_index, size).each do |i|
+      new_cost = current_data[:cost] + graph[i]
+      neighbor_node = path_data[i]
+
+      next if neighbor_node[:finalized]
+      next if neighbor_node[:cost] < new_cost
+
+      path_data[i] = {
+        cost: new_cost,
+        path: current_data[:path].clone.push(i),
+        finalized: false
+      }
+
+      candidate_queue.push(i, calc_priority(new_cost, i, size))
+    end
+
+    iterations += 1
+    puts "#{iterations} / #{goal} | #{((iterations.to_f / goal)*100).round(3)}%"
+
+    break if current_index == graph.length - 1
+  end
+  path_data[goal][:path]
 end
-
-# puts "m(0,99)  => #{manhattan(0,99)}"
-# puts "m(1,99)  => #{manhattan(1,99)}"
-# puts "m(10,99) => #{manhattan(10,99)}"
-# puts
-# puts "m(99,99) => #{manhattan(99,99)}"
-
-# exit
-
-# (0..9).each do |y|
-#   (0..9).each do |x|
-#     i = (y*10)+x
-#     print "#{manhattan(i, 99).to_s.rjust(3, '0')} "
-#   end
-#   puts
-# end
-
-# exit
 
 nodes, size = slurp_and_parse($args[:file])
 nodes, size = scale_up(nodes, size, 5)
-path = dijkstra_spt(nodes, size)
+path = a_star(nodes, size)
 puts path.to_s
 puts path.map { |i| nodes[i] }.sum
