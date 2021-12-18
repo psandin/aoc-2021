131c5a81835235ce2c154d128d7418f2f1f8a1cc
Prime <isabel@thesandins.net>
2021-12-18 00:10:34 -0800

    17p1

--- 2021-12-17/part_2.rb
+++ 2021-12-17/part_2.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
-require 'pp'
 require 'optparse'
+require 'term/ansicolor'
+
+class String
+  include Term::ANSIColor
+end
 
 $args = {
   file: "#{File.dirname(__FILE__)}/input"
@@ -23,8 +27,77 @@ def slurp(path)
   input_str = input_fh.read
   input_fh.close
 
-  input_str.split(/\n/)
+  input_str.split(/\n/).first
 end
 
-raw_lines = slurp($args[:file])
-puts raw_lines.to_s
+def parse_target (line)
+  _, cords = line.split(': x=')
+  x_raw, y_raw = cords.split(', y=')
+  x = x_raw.split('..').map(&:to_i).sort
+  y = y_raw.split('..').map(&:to_i).sort
+  { x: (x[0]..x[1]), y: (y[0]..y[1]) }
+end
+
+def xmatch (point, target)
+  target[:x].include?(point[:x])
+end
+
+def ymatch (point, target)
+  target[:y].include?(point[:y])
+end
+
+def in_target (point, target)
+   xmatch(point, target) && ymatch(point, target)
+end
+
+def display_path (data)
+  data[:points].each do |p|
+    x = p[:x].to_s
+    x = x.bold if p[:xmatch]
+    y = p[:y].to_s
+    y = y.bold if p[:ymatch]
+    puts "x => #{x} y => #{y} hit => #{p[:hit]}"
+  end
+  puts "max y => #{data[:maxy]}"
+  puts '================'
+end
+
+def calc_x_range (target)
+  minx = Math.sqrt(((2*target.min) + 0.25) - 1/2).ceil - 2
+  maxx = Math.sqrt(((2*target.max) + 0.25) - 1/2).floor + 2
+  minx..maxx
+end
+
+def trace_shot(initx, inity, target)
+  history = {}
+  history[:points] = [{ x: 0, y: 0}]
+  history[:maxy] = 0
+  while true
+    last = history[:points].reverse.first
+    current = { x: last[:x] + initx, y: last[:y] + inity }
+    current[:ymatch] = ymatch(current, target)
+    current[:xmatch] = xmatch(current, target)
+    current[:hit] = in_target(current, target)
+    history[:hit] = true if current[:hit]
+    history[:maxy] = current[:y] if current[:y] > history[:maxy]
+    history[:points].push(current)
+    inity -= 1
+    initx += ((initx > 0) ? -1 : 1) if !initx.zero?
+    break if current[:y] < target[:y].min
+    break if current[:x] > target[:x].max
+  end
+  history
+end
+
+line = slurp($args[:file])
+target_range = parse_target(line)
+puts target_range.to_s
+xrange = calc_x_range(target_range[:x])
+runs = []
+xrange.each do |x|
+  (0..200).each do |y|
+    runs.push(trace_shot(x, y, target_range))
+  end
+end
+
+runs.select { |r| r[:hit] }.sort_by {|r| -r[:maxy]}.first(2).each {|r| display_path(r)}
