07666b3be13ed4220532fede8df1c17e0d5efc33
Prime <isabel@thesandins.net>
2021-12-20 10:49:47 -0800

    Day 20 part 1, also bonus GoL implementation

--- 2021-12-20/part_1.rb
+++ 2021-12-20/part_1.rb
@@ -26,5 +26,72 @@ def slurp(path)
   input_str.split(/\n/)
 end
 
-raw_lines = slurp($args[:file])
-puts raw_lines.to_s
+def compute_gol_rules
+  (0..511).map do |v|
+    live_cell = !(v & 16).zero?
+    neighbor_count = (v & 495).to_s(2).count('1')
+    (live_cell && (2..3).include?(neighbor_count)) || (!live_cell && neighbor_count == 3)
+  end
+end
+
+def parse_input(lines)
+  rules = lines.shift.chars.map { |c| c == '#' }
+  lines.shift
+  map = []
+  lines.each_with_index do |r, y|
+    r.chars.each_with_index do |c, x|
+      map.push([x, y]) if c == '#'
+    end
+  end
+  [map, rules]
+end
+
+def display_rules(rules)
+  puts rules.map { |b| b ? '#' : '.' }.join
+end
+
+def tick(map, rules)
+  new_map = []
+  bounds = get_bounds(map)
+  (bounds[:y][0]..bounds[:y][1]).each do |y|
+    (bounds[:x][0]..bounds[:x][1]).each do |x|
+      new_map.push([x, y]) if rules[index_for_cell(map, [x, y])]
+    end
+  end
+  new_map
+end
+
+def get_bounds(map)
+  xs = map.map { |e| e[0] }
+  ys = map.map { |e| e[1] }
+  { x: [xs.min - 1, xs.max + 1], y: [ys.min - 1, ys.max + 1] }
+end
+
+def display_map(map)
+  bounds = get_bounds(map)
+  (bounds[:y][0]..bounds[:y][1]).each do |y|
+    (bounds[:x][0]..bounds[:x][1]).each do |x|
+      print map.include?([x, y]) ? '#' : '.'
+    end
+    puts
+  end
+end
+
+def index_for_cell(map, point)
+  bstr = ''
+  (-1..1).each do |ym|
+    (-1..1).each do |xm|
+      bstr += map.include?([point[0] + xm, point[1] + ym]) ? '1' : '0'
+    end
+  end
+  bstr.to_i(2)
+end
+
+map, rules = parse_input(slurp($args[:file]))
+display_map(map)
+2.times do
+  puts
+  map = tick(map, rules)
+  display_map(map)
+end
+puts map.length
