ba72e185bbe7d3996dc776ab0efeb45182646d25
Prime <isabel@thesandins.net>
2022-01-04 17:17:26 -0800

    Day 21p1

--- 2021-12-21/part_1.rb
+++ 2021-12-21/part_1.rb
@@ -26,5 +26,38 @@ def slurp(path)
   input_str.split(/\n/)
 end
 
+def parse_inputs(lines)
+  lines.map { |l| l.split(/: /).reverse.first.to_i }
+end
+
+def play_game(pos)
+  $rc = 0
+  scores = [0, 0]
+  while scores.all? { |e| e < 1000 }
+    0.upto(1) do |p|
+      rolls = [roll, roll, roll]
+      pos[p] += rolls.sum
+      pos[p] = pos[p].digits.first if pos[p] > 9
+      pos[p] = 10 if pos[p].zero?
+      scores[p] += pos[p]
+      if $args[:verbose]
+        puts "Player #{p + 1} rolls #{rolls} and moves to space #{pos[p]} for a total score of #{scores[p]}"
+      end
+      break if scores[p] >= 1000
+    end
+  end
+  puts scores.to_s
+  puts $rc
+  prod = scores.min * $rc
+  puts prod
+end
+
+def roll
+  res = ($rc % 100) + 1
+  $rc += 1
+  res
+end
+
 raw_lines = slurp($args[:file])
-puts raw_lines.to_s
+starting_pos = parse_inputs(raw_lines)
+play_game(starting_pos)
