6697dcd209fef667a084a06c889f8b82f9165d23
Prime <isabel@thesandins.net>
2022-01-09 11:46:53 -0800

    Early skeleton of 25-1

--- 2021-12-25/part_1.rb
+++ 2021-12-25/part_1.rb
@@ -26,5 +26,38 @@ def slurp(path)
   input_str.split(/\n/)
 end
 
+def init_map(lines)
+  y_max = lines.count
+  x_max = lines[0].chars.count
+  smap = {}
+  lines.each_with_index do |l, y|
+    l.chars.each_with_index do |c,x|
+      smap[[x,y]] = c
+    end
+  end
+  return {
+    y: y_max,
+    x: x_max,
+    smap: smap,
+  }
+end
+
+def tick(map)
+  omap = map[:smap]
+  (0..map[:x]).each do |x|
+    (0..map[:y]).each do |y|
+      if omap[[x,y]] == '>'
+
+      end
+    end
+  end
+end
+
+def render_map(map)
+
+end
+
 raw_lines = slurp($args[:file])
 puts raw_lines.to_s
+sea_map = init_map(raw_lines)
+puts sea_map.to_s
