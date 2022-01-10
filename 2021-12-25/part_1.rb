8bef05659711ebd05fd16f9baed960179c545c8f
Prime <isabel@thesandins.net>
2022-01-10 10:46:56 -0800

    Day 25 part 1

--- 2021-12-25/part_1.rb
+++ 2021-12-25/part_1.rb
@@ -36,28 +36,77 @@ def init_map(lines)
     end
   end
   return {
-    y: y_max,
-    x: x_max,
+    y: y_max - 1,
+    x: x_max - 1,
     smap: smap,
   }
 end
 
 def tick(map)
+  u = 0
+  u += east_tick(map)
+  u += south_tick(map)
+end
+
+def east_tick(map)
   omap = map[:smap]
+  updates = []
   (0..map[:x]).each do |x|
     (0..map[:y]).each do |y|
       if omap[[x,y]] == '>'
-
+        nx = x + 1
+        nx = 0 if nx > map[:x]
+        if omap[[nx,y]] == '.'
+          updates.push([[x,y],'.'])
+          updates.push([[nx,y],'>'])
+        end
       end
     end
   end
+  updates.each {|u| omap[u[0]] = u[1] }
+  updates.count / 2
+end
+
+def south_tick(map)
+  omap = map[:smap]
+  updates = []
+  (0..map[:x]).each do |x|
+    (0..map[:y]).each do |y|
+      if omap[[x,y]] == 'v'
+        ny = y + 1
+        ny = 0 if ny > map[:y]
+        if omap[[x,ny]] == '.'
+          updates.push([[x,y],'.'])
+          updates.push([[x,ny],'v'])
+        end
+      end
+    end
+  end
+  updates.each {|u| omap[u[0]] = u[1] }
+  updates.count / 2
 end
 
 def render_map(map)
-
+  omap = map[:smap]
+  (0..map[:y]).each do |y|
+    (0..map[:x]).each do |x|
+      print omap[[x,y]]
+    end
+    puts
+  end
 end
 
 raw_lines = slurp($args[:file])
 puts raw_lines.to_s
 sea_map = init_map(raw_lines)
 puts sea_map.to_s
+render_map(sea_map)
+updates = 1
+rounds = 0
+until updates.zero?
+  rounds +=1
+  updates =  tick(sea_map)
+  render_map(sea_map)
+  puts updates
+end
+puts rounds
