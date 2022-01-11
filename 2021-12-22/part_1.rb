fe3ab17567d1d07133ceb4a13c07c1649a5d0e83
Prime <isabel@thesandins.net>
2022-01-10 17:00:13 -0800

    day 22 part 1

--- 2021-12-22/part_1.rb
+++ 2021-12-22/part_1.rb
@@ -26,5 +26,51 @@ def slurp(path)
   input_str.split(/\n/)
 end
 
+$range_min = -50
+$ramge_max = 50
+
+def parse_steps(lines)
+  instructions = []
+  lines.each do |l|
+    opv, ranges = l.split(/ /)
+    parsed_ranges = ranges.split(/,/).map do |r|
+      axis, raw_values = r.split(/=/)
+      values = raw_values.split(/\.\./).map(&:to_i)
+      [axis.to_sym, values]
+    end
+    next if parsed_ranges[0][1].min > $ramge_max
+    next if parsed_ranges[0][1].max < $range_min
+    next if parsed_ranges[1][1].min > $ramge_max
+    next if parsed_ranges[1][1].max < $range_min
+    next if parsed_ranges[2][1].min > $ramge_max
+    next if parsed_ranges[2][1].max < $range_min
+    parsed_ranges.push([:op, (opv == 'on')])
+    instructions.push(Hash[parsed_ranges])
+  end
+  instructions
+end
+
+def execute_instructions(instructions)
+  state = {}
+  instructions.each do |e|
+    (e[:x][0]..e[:x][1]).each do |x|
+      (e[:y][0]..e[:y][1]).each do |y|
+        (e[:z][0]..e[:z][1]).each do |z|
+          state[[x,y,z]] = e[:op]
+        end
+      end
+    end
+  end
+  state
+end
+
+def count_bits(state)
+  state.map { |k,v| v }.select { |e| e  }.count
+end
+
 raw_lines = slurp($args[:file])
-puts raw_lines.to_s
+# puts raw_lines.to_s
+instructions = parse_steps(raw_lines)
+puts instructions
+state = execute_instructions(instructions)
+puts state.map { |k,v| v }.select { |e| e  }.count
